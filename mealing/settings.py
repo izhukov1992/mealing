2ae5c0c9ff45d14f1ab04d48f662173231eb6c2e
Ilya Zhukov <izhukov1992@gmail.com>
2016-07-07 15:39:31 +0300

    Deploy on Heroku

--- mealing/settings.py
+++ mealing/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -80,6 +81,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+DATABASES['default'] =  dj_database_url.config()
 
 
 # Password validation
